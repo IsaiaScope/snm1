1c54e0b3b50bb471f045cd400df21e4fa52601b1
Isaia Riva <isaia.riva@finconsgroup.com>
2024-06-18 22:26:15 +0200

    fix connection to local mongo compass and suppress  strictQuery warning

--- project/backend/app.js
+++ project/backend/app.js
@@ -15,14 +15,26 @@ app.use('/api/users', userRoutes);
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '../public')));
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}).catch(err => {
-  console.error('Error connecting to MongoDB:', err.message);
-});
+
+
+// When strictQuery is set to true, Mongoose will ignore any fields in a query that are not defined in the schema. This is the default behavior in Mongoose 6 and earlier.
+
+// When strictQuery is set to false, Mongoose will allow queries to include fields that are not defined in the schema. This will be the default behavior in Mongoose 7 and later.
+mongoose.set("strictQuery", true);
+
+
+
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('Connected to MongoDB');
+		app.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+	})
+	.catch(err => {
+		console.error('Error connecting to MongoDB:', err.message);
+	});
