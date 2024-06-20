46671df91aa177bae3fb06d13b00cebf300af4b6
Isaia Riva <isaia.riva@finconsgroup.com>
2024-06-20 18:36:00 +0200

    refactor: Update app.js to improve code structure and error handling

--- project/backend/app.js
+++ project/backend/app.js
@@ -8,54 +8,82 @@ const jwt = require('jsonwebtoken');
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use((res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+	next();
+});
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB:', err);
-});
-
-app.use(bodyParser.json());
+mongoose.set('strictQuery', true);
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('Connected to MongoDB');
+	})
+	.catch(err => {
+		console.error('Error connecting to MongoDB:', err);
+	});
 
 const userSchema = new mongoose.Schema({
-  nome: String,
-  cognome: String,
-  username: String,
-  email: String,
-  password: String,
-  eta: Number,
-  preferenzeMusicali: [String],
-  gruppiMusicali: [String]
+	nome: String,
+	cognome: String,
+	username: { type: String },
+	email: { type: String, required: true, unique: true },
+	password: String,
+	eta: Number,
+	preferenzeMusicali: [String],
+	gruppiMusicali: [String],
 });
 
 const User = mongoose.model('User', userSchema);
 
 app.post('/api/users/register', async (req, res) => {
-  const { nome, cognome, username, email, password, eta, preferenzeMusicali, gruppiMusicali } = req.body;
+	const {
+		nome,
+		cognome,
+		username,
+		email,
+		password,
+		eta,
+		preferenzeMusicali,
+		gruppiMusicali,
+	} = req.body;
+	console.log(`🧊 ~ req.body: `, req.body);
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({
-      nome,
-      cognome,
-      username,
-      email,
-      password: hashedPassword,
-      eta,
-      preferenzeMusicali,
-      gruppiMusicali
-    });
-    await newUser.save();
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (err) {
-    res.status(500).json({ message: 'Error registering user', error: err });
-  }
+	try {
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const newUser = new User({
+			nome,
+			cognome,
+			username,
+			email,
+			password: hashedPassword,
+			eta,
+			preferenzeMusicali,
+			gruppiMusicali,
+		});
+
+		const existingUser = await User.findOne({ email });
+		if (existingUser) {
+			return res.status(400).json({ message: 'email already exists' });
+		}
+
+		await newUser.save();
+		res.status(201).json({ message: 'User registered successfully' });
+	} catch (err) {
+		res.status(500).json({ message: 'Error registering user', error: err });
+	}
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
